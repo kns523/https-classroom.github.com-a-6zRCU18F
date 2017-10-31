76ab1b33b42c556b9ca90b90012c717ac9fce319
Keeghan Sweeney <KeeghanSweeney@keeghans-mbp.ad.wlu.edu>
2017-10-31 16:08:16 -0400

    fds

--- script.js
+++ script.js
@@ -6,15 +6,16 @@ function areaTriangle(b, h) {
     return areaT;
 }
 
-var areaT = ("The area of a triangle of this base and height is " + areaTriangle(b, h));
+var areaT = ("The area of a triangle with a base of" + this.b + "and height of " + this.h + " is " + areaTriangle(b, h));
 
 document.getElementById("1").innerHTML = areaT;
 
 function calcHypotenuse(b, h) {
     var calcH = (Math.sqrt(b*b + h*h));
     return calcH;  
+
 }
 
-var calcH = ("The hypotenuse of a triangle with a base of" + this.b + " and a height of" + this.h + "is " + calcHypotenuse(b, h));
+var calcH = ("The hypotenuse of a triangle with a base of " + this.b + " and a height of " + this.h + "is " + calcHypotenuse(b, h));
 
 document.getElementById("2").innerHTML = calcH;
