ffd7cd8811e02c2e1ba678796514fa4665acee33
Keeghan Sweeney <KeeghanSweeney@keeghans-mbp.ad.wlu.edu>
2017-10-31 18:41:54 -0400

    nkl

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
+
 var b = 30;    //Base
 var h = 40;  //Height
 
 function areaTriangle(b, h) {
-    var calcArea = 1/2 * (b * h);
-    return calcArea;
+    var areaT = 1/2 * (b * h);
+    return areaT;
 }
 
-var calcArea = ("The area of a triangle with a base of " + this.b +
- " and a height of " + this.h + " is " + areaTriangle(b, h) + ".");
+var areaT = ("The area of a triangle with a base of " + this.b + " and a height of " + this.h + " is " + areaTriangle(b, h) + ".");
 
-document.getElementById("1").innerHTML = calcArea;
+document.getElementById("1").innerHTML = areaT;
 
 function calcHypotenuse(b, h) {
     var calcH = (Math.sqrt(b*b + h*h));
     return calcH;  
+
 }
 
-var calcH = ("The length of the hypotenuse of a triangle with a base of " + this.b +
- " and a height of " + this.h + " is " + calcHypotenuse(b, h) + ".");
+var calcH = ("The hypotenuse of a triangle with a base of " + this.b + " and a height of " + this.h + " is " + calcHypotenuse(b, h) + ".");
 
 document.getElementById("2").innerHTML = calcH;
 
